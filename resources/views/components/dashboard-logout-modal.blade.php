e1f9b3994cafb3814670887ce640d31048633dc2
ATik HaSan <atikhasan2700@gmail.com>
2025-04-02 10:49:21 +0600

    Auto update

--- resources/views/components/dashboard-logout-modal.blade.php
+++ resources/views/components/dashboard-logout-modal.blade.php
@@ -1,17 +1,15 @@
 <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-<div class="modal-dialog" role="document">
-    <div class="modal-content">
-        <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-            <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">×</span>
-            </button>
-        </div>
-        <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-        <div class="modal-footer">
-            <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-            <a class="btn btn-primary" href="#">Logout</a>
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content border-0">
+            <div class="modal-header d-flex align-items-center">
+                <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                <button class="close" type="button" data-dismiss="modal" aria-label="Close"><i aria-hidden="true" class="h6 fa-regular fa-circle-xmark"></i></button>
+            </div>
+            <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+            <div class="modal-footer">
+                <button class="btn btn-sm btn-secondary px-3" type="button" data-dismiss="modal">Cancel</button>
+                <a class="btn btn-sm btn-primary px-4" href="#">Logout</a>
+            </div>
         </div>
     </div>
-</div>
 </div>
--- resources/views/components/dashboard-modal.blade.php
+++ resources/views/components/dashboard-modal.blade.php
@@ -1,3 +1,21 @@
-<div>
-    <!-- Very little is needed to make a happy life. - Marcus Aurelius -->
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">Launch demo modal</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header d-flex align-items-center">
+                <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                <button class="close" type="button" data-dismiss="modal" aria-label="Close"><i aria-hidden="true" class="h6 fa-regular fa-circle-xmark"></i></button>
+            </div>
+            <div class="modal-body">
+                ...
+            </div>
+            <div class="modal-footer">
+                <button class="btn btn-sm btn-secondary px-3" type="button" data-dismiss="modal">Cancel</button>
+                <a class="btn btn-sm btn-primary px-4" href="#">Save changes</a>
+            </div>
+        </div>
+    </div>
 </div>
